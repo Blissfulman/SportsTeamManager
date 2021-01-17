27b0b9210aa7a7f7071404546dd2e8be958e2c7a
Evgeny Novgorodov <greatly01@ya.ru>
2021-01-17 22:35:39 +0500

    Initial Commit

---
+++ SportsTeamManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsTeamManager
+//
+//  Created by User on 17.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

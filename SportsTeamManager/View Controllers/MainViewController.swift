d442d9ab37579d34f6a41fdddeaa6618c289ae88
Evgeny Novgorodov <greatly01@ya.ru>
2021-01-18 17:00:32 +0500

    CoreDataManager was added + refactoring

---
+++ SportsTeamManager/View Controllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  SportsTeamManager
+//
+//  Created by User on 17.01.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Properties
+    var dataManager: CoreDataManager!
+    
+    // MARK: - Lifecycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+}

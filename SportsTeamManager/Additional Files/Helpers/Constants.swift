2ad28c9a396c00f7b6fceccdaf1b6c193edf765f
Evgeny Novgorodov <greatly01@ya.ru>
2021-01-29 23:11:34 +0500

    Icons were added

--- SportsTeamManager/Additional Files/Helpers/Constants.swift
+++ SportsTeamManager/Additional Files/Helpers/Constants.swift
@@ -1,9 +1,15 @@
 //
-//  DataConstants.swift
+//  Constants.swift
 //  SportsTeamManager
 //
-//  Created by User on 22.01.2021.
+//  Created by User on 29.01.2021.
 //
+
+import UIKit
+
+enum Color {
+    static let main = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+}
 
 enum DataConstants {
     
